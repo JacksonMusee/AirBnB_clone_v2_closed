9c3d20a7102bd874b958962031c1223131522d69
JacksonMusee <jacksonmutisya94@gmail.com>
2024-04-10 18:51:11 +0300

    Push

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -25,7 +25,7 @@ if [ -L "/data/web_static/current" ]; then
     sudo rm "/data/web_static/current"
 fi
 
-sudo ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+sudo ln -s "/data/web_static/releases/test" "/data/web_static/current"
 
 sudo chown -R ubuntu:ubuntu /data/
 
