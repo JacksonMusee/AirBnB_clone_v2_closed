09a039919ecf1e1704778b950365388099722d47
JacksonMusee <jacksonmutisya94@gmail.com>
2024-04-10 19:37:59 +0300

    Push

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,57 +1,57 @@
-#!/usr/bin/env bash
-#Script that sets up your web servers for the deployment of web_static
+#!/bin/bash
 
+# Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update
-    sudo aptget install -y nginx
+    sudo apt-get install -y nginx
 fi
 
-sudo service nginx start
-
+# Create necessary directories
 sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo touch /data/web_static/releases/test/index.html
 
-sudo echo "<html>
+# Create a fake HTML file
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+<html>
   <head>
+    <title>Test Page</title>
   </head>
   <body>
-    Holberton School
+    <h1>Hello, this is a test page</h1>
+    <p>This is just a test to verify Nginx configuration.</p>
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>
+EOF
 
+# Create symbolic link
 if [ -L "/data/web_static/current" ]; then
     sudo rm "/data/web_static/current"
 fi
+sudo ln -s /data/web_static/releases/test /data/web_static/current
 
-sudo ln -s "/data/web_static/releases/test" "/data/web_static/current"
-
+# Give ownership to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/http {/a \
-    \
-    server {\
-        server_name cityspaces.tech;\
-        \
-        return 301 $scheme://localhost$request_uri;\
-    }\
-    \
-    server {\
-        server_name www.cityspaces.tech;\
-        \
-        return 301 $scheme://localhost$request_uri;\
-    }\
-    \
-    server {\
-        server_name localhost;\
-        \
-        location /hbnb_static {\
-            alias /data/web_static/current;\
-        }\
-        \
-        location / {\
-            return 404;\
-        }\
-    }\
-    ' /etc/nginx/nginx.conf
+# Update Nginx configuration
+sudo tee /etc/nginx/sites-available/hbnb_static > /dev/null <<EOF
+server {
+    listen 80;
+    listen [::]:80;
 
-sudo service nginx restart
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+
+    location / {
+        return 404;
+    }
+}
+EOF
+
+# Create symbolic link to enable the new configuration
+sudo ln -sf /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+
+# Restart Nginx
+sudo systemctl restart nginx
+
