d688dd4af2180b377f52136a22f0e2b892d94414
JacksonMusee <jacksonmutisya94@gmail.com>
2024-04-10 19:27:37 +0300

    Push

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 #Script that sets up your web servers for the deployment of web_static
 
-sudo apt-get update
-sudo aptget install -y nginx
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo aptget install -y nginx
+fi
+
+sudo service nginx start
 
 sudo mkdir -p /data/web_static/{releases/test,shared}
 sudo touch /data/web_static/releases/test/index.html
@@ -24,6 +28,18 @@ sudo ln -s "/data/web_static/releases/test" "/data/web_static/current"
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/http {/a \
+    \
+    server {\
+        server_name cityspaces.tech;\
+        \
+        return 301 $scheme://localhost@request_uri;\
+    }\
+    \
+    server {\
+        server_name www.cityspaces.tech;\
+        \
+        return 301 $scheme://localhost@request_uri;\
+    }\
     \
     server {\
         server_name localhost;\
