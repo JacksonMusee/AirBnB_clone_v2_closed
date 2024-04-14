68a898ab6446a923df908d7457043f7ff4212194
JacksonMusee <jacksonmutisya94@gmail.com>
2024-04-14 20:58:48 +0300

    Push

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,9 +9,10 @@ from fabric.api import env
 from os.path import exists
 
 
-env.hosts = ["100.26.247.135","	18.235.248.212"]
+env.hosts = ["100.26.247.135", "18.235.248.212"]
 env.key_filename = '~/.ssh/school'
 env.user = 'ubuntu'
+
 
 def do_deploy(archive_path):
     """
@@ -30,11 +31,13 @@ def do_deploy(archive_path):
         remote_archive_path = "/tmp/" + archive_name
 
         run(f"mkdir -p /data/web_static/releases/{rm_extension}")
-        run(f"tar -xzf {remote_archive_path} -C /data/web_static/releases/{rm_extension}")
+        run(f"tar -xzf {remote_archive_path} -C /data/web_static/\
+              releases/{rm_extension}")
         run(f"rm -rf {remote_archive_path}")
 
         run("rm -rf /data/web_static/current")
-        run(f"ln -s /data/web_static/releases/{rm_extension} /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/{rm_extension} /data/\
+              web_static/current")
 
         return True
 
