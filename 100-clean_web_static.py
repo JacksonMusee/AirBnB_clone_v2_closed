4728a7ba4b057f0410e6372274afc37a450f2b2b
JacksonMusee <jacksonmutisya94@gmail.com>
2024-06-05 07:12:26 -0700

    Push

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -89,7 +89,7 @@ def do_clean(number=0):
     if number == 0:
         number = 1
 
-    point = number + 1
+    point = number
 
     local("ls -lt versions | tail -n +{0} | awk '{{print $9}}' | xargs -I {{}} rm -r versions/{{}}".format(point))
 
