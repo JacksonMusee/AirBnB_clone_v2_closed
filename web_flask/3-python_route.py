cceed0cbc96e7565b9cc24dc189d6f589560bc95
JacksonMusee <jacksonmutisya94@gmail.com>
2024-06-11 06:04:14 +0000

    Push

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+Creates a flask app
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''Home route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Home route'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    '''Takes a text and uses it'''
+    clean_text = text.replace('_', ' ')
+    return "C " + clean_text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    '''Text with a default value'''
+    clean_txt = text.replace('_', ' ')
+    return "Python " + clean_txt
+
+if __name__ == '__main__':
+    app.run()
